b584a10e459606f42d73c6d347717cc7e8163ebb
Andrew <arhorn@smcm.edu>
2018-12-21 02:25:00 +0700

    finishe adding front-end projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,19 +5,19 @@ import frontEndProjectsInfo from './projectsInfo/frontEndProjectsInfo';
 
 const Projects = () => (
   <div>
-  <h1 style={{ textAlign: "center"}}>full stack projects</h1>
+    <h1 style={{ textAlign: "center"}}>full stack projects</h1>
     <section id="full-stack-projects" className="full-stack-projects__container">
       {
         fullStackProjectsInfo.map(projectInfo => <SingleProject {...projectInfo} />)
       }
     </section>
-  <h1 style={{ textAlign: "center"}}>front end projects</h1>
-
+    <h1 style={{ textAlign: "center"}}>front end projects</h1>
     <section id="front-end-projects" className="front-end-projects__container">
       {
         frontEndProjectsInfo.map(projectInfo => <SingleProject {...projectInfo} />)
       }
     </section>
+    <h1 style={{ textAlign: "center"}}>api projects</h1>
     <section id="api-projects" className="projects__container">
     
     </section>
--- src/components/Projects/projectsInfo/frontEndProjectsInfo.js
+++ src/components/Projects/projectsInfo/frontEndProjectsInfo.js
@@ -1,6 +1,15 @@
 import gameOfLifeImage from './images/game-of-life.png';
 import recipeListImage from './images/recipe-list.png';
 import markdownPreviewerImage from './images/markdown-previewer.png';
+import simonImage from './images/simon.png';
+import ticTacToeImage from './images/tic-tac-toe.png';
+import pomodoroClockImage from './images/pomodoro-clock.png';
+import calculatorImage from './images/calculator.png';
+import whosStreamingOnTwitchInage from './images/whos-on-twitch.png';
+import wikipediaViewerImage from './images/wikipedia-viewer.png';
+import whatsTheWeatherImage from './images/whats-the-weather.png';
+import randomZhuangziQuoteMachineImage from './images/random-zhuangzi-quote-machine.png';
+import tributePageForDengXiaopingImage from './images/tribute-page-to-deng-xiaoping.png';
 
 export default [
   {
@@ -28,11 +37,67 @@ export default [
     github: 'https://github.com/sophyphreak/markdown-previewer'
   },
   {
-    title: 'markdown-previewer',
+    title: 'simon',
     subtitle: 'react + create-react-app | digitalocean + dokku | bootstrap + reactstrap | git',
-    image: recipeListImage,
-    text: 'you can write in markdown and instantly see the result',
-    demo: 'https://markdown-previewer.andrew-horn-portfolio.life/',
-    github: 'https://github.com/sophyphreak/markdown-previewer'
+    image: simonImage,
+    text: 'play the classic simon game in your browser',
+    demo: 'https://simon.andrew-horn-portfolio.life/',
+    github: 'https://github.com/sophyphreak/simon'
   },
+  {
+    title: 'tic-tac-toe',
+    subtitle: 'react + create-react-app | digitalocean + dokku | bootstrap + reactstrap | git',
+    image: ticTacToeImage,
+    text: 'play the classic simon game in your browser',
+    demo: 'https://tic-tac-toe.andrew-horn-portfolio.life/',
+    github: 'https://github.com/sophyphreak/tic-tac-toe'
+  },
+  {
+    title: 'pomodoro-clock',
+    subtitle: 'react + create-react-app | digitalocean + dokku | bootstrap + reactstrap | git',
+    image: pomodoroClockImage,
+    text: 'manage your busy time better. and take breaks!!',
+    demo: 'https://pomodoro-clock.andrew-horn-portfolio.life/',
+    github: 'https://github.com/sophyphreak/pomodoro-clock'
+  },
+  {
+    title: 'calculator',
+    subtitle: 'react + create-react-app | digitalocean + dokku | bootstrap + reactstrap | git',
+    image: calculatorImage,
+    text: 'do the numbers',
+    demo: 'https://calculator.andrew-horn-portfolio.life/',
+    github: 'https://github.com/sophyphreak/calculator'
+  },
+  {
+    title: 'wikipedia-viewer',
+    subtitle: 'react + create-react-app | digitalocean + dokku | bootstrap + reactstrap | git',
+    image: wikipediaViewerImage,
+    text: 'idk something about wikipedia',
+    demo: 'https://wikipedia-viewer.andrew-horn-portfolio.life/',
+    github: 'https://github.com/sophyphreak/wikipedia-viewer'
+  }, 
+  {
+    title: 'whats-the-weather-like',
+    subtitle: 'react + create-react-app | digitalocean + dokku | bootstrap + reactstrap | git',
+    image: whatsTheWeatherImage,
+    text: 'see what the weather\'s like. may require refresh',
+    demo: 'https://whats-the-weather-like.andrew-horn-portfolio.life/',
+    github: 'https://github.com/sophyphreak/whats-the-weather-like'
+  },
+  {
+    title: 'random-zhuangzi-quote-machine',
+    subtitle: 'react + create-react-app | digitalocean + dokku | bootstrap + reactstrap | git',
+    image: randomZhuangziQuoteMachineImage,
+    text: 'get some wisdom from ancient wise words',
+    demo: 'https://random-zhuangzi-quote-machine.andrew-horn-portfolio.life/',
+    github: 'https://github.com/sophyphreak/random-zhuangzi-quote-machine'
+  },
+  {
+    title: 'tribute-page-for-deng-xiaoping',
+    subtitle: 'react + create-react-app | digitalocean + dokku | bootstrap + reactstrap | git',
+    image: tributePageForDengXiaopingImage,
+    text: 'learn about communist china\'s greatest leader',
+    demo: 'https://tribute-page-for-deng-xiaoping.andrew-horn-portfolio.life/',
+    github: 'https://github.com/sophyphreak/tribute-page-for-deng-xiaoping'
+  }
 ]
