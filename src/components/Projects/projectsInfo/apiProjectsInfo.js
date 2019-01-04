73b0a55c57acb987102da1c4e870c2c9fe224259
Andrew <arhorn@smcm.edu>
2019-01-04 22:51:48 +0700

    added issue-tracker

--- src/components/Projects/projectsInfo/apiProjectsInfo.js
+++ src/components/Projects/projectsInfo/apiProjectsInfo.js
@@ -3,6 +3,7 @@ import metricImperialConverterImage from './images/metric-imperial-converter.png
 import requestHeaderImage from './images/request-header.png';
 import fileMetadataImage from './images/file-metadata.png';
 import exerciseTrackerImage from './images/exercise-tracker.png';
+import issueTrackerImage from './images/issue-tracker.png';
 
 export default [
   {
@@ -45,8 +46,17 @@ export default [
     subtitle:
       "POST to create new users and their exercises, then GET a list of a user's exercises",
     text:
-      'node + expres | body-parser | cors | dotenv | lodash | moment | mongodb + mongoose | path | shortid | expect + mocha + supertest | digitalocean + dokku | git',
+      'node + express | body-parser | cors | dotenv | lodash | moment | mongodb + mongoose | path | expect + mocha + supertest | husky + lint-staged + prettier | digitalocean + dokku | git',
     demo: 'https://exercise-tracker.andrew-horn-portfolio.life/',
     github: 'https://github.com/sophyphreak/exercise-tracker'
+  },
+  {
+    title: 'issue-tracker',
+    image: issueTrackerImage,
+    subtitle: 'has all CRUD operations with REST API',
+    text:
+      'node + express | body-parser | cors | dotenv | helmet | lodash | moment | mongodb + mongoose | path | chai + chai-http + mocha | husky + lint-staged + prettier | digitalocean + dokku | zombie | git',
+    demo: 'https://issue-tracker.andrew-horn-portfolio.life/',
+    github: 'https://github.com/sophyphreak/issue-tracker'
   }
 ];
