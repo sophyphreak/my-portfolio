edb0b201c8044ff0d83aea41904a1bc401cdbe4f
Andrew <arhorn@smcm.edu>
2019-01-30 13:20:18 +0700

    moved tribute-page-to-deng-xiaoping to gatsby and netlify

--- src/components/Projects/projectsInfo/frontEndProjects/individualProjects/tributePageForDengXiaoping.js
+++ src/components/Projects/projectsInfo/frontEndProjects/individualProjects/tributePageForDengXiaoping.js
@@ -2,7 +2,7 @@ export default {
   title: 'tribute-page-for-deng-xiaoping',
   imagePath: 'front-end/tribute-page-to-deng-xiaoping.png',
   subtitle: "learn about communist china's greatest leader",
-  text: 'html + css | digitalocean + dokku | git',
-  demo: 'https://tribute-page-for-deng-xiaoping.andrew-horn-portfolio.life/',
+  text: 'html + css + react + gatsby | netlify | git',
+  demo: 'https://tribute-page-to-deng-xiaoping-andrew-horn.netlify.com/',
   github: 'https://github.com/sophyphreak/tribute-page-for-deng-xiaoping'
 };
