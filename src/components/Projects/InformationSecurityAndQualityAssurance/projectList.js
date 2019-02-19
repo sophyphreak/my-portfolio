7d4cbd2dfb28397037b4041379c42f5ace54fc61
Andrew <arhorn@smcm.edu>
2019-02-20 01:46:38 +0700

    added stock-price-checker to portfolio

--- src/components/Projects/InformationSecurityAndQualityAssurance/projectList.js
+++ src/components/Projects/InformationSecurityAndQualityAssurance/projectList.js
@@ -1,5 +1,11 @@
+import stockPriceChecker from './individualProjects/stockPriceChecker';
 import personalLibrary from './individualProjects/personalLibrary';
 import issueTracker from './individualProjects/issueTracker';
 import metricImperialConverter from './individualProjects/metricImperialConverter';
 
-export default [personalLibrary, issueTracker, metricImperialConverter];
+export default [
+  stockPriceChecker,
+  personalLibrary,
+  issueTracker,
+  metricImperialConverter
+];
