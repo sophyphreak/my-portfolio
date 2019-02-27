24230d0b434d4e7c3d0b780dcf2f3d16fde5a9bd
Andrew <arhorn@smcm.edu>
2019-02-27 14:36:23 -0600

    updated description for anon-message-board

--- src/components/Projects/InformationSecurityAndQualityAssurance/individualProjects/anonMessageBoard.js
+++ src/components/Projects/InformationSecurityAndQualityAssurance/individualProjects/anonMessageBoard.js
@@ -1,7 +1,8 @@
 export default {
   title: 'anon-message-board',
   imagePath: 'informationSecurityAndQualityAssurance/anon-message-board.png',
-  subtitle: 'you can create threads and replies on a message board',
+  subtitle:
+    'you can create threads and replies on a message board. has full CRUD functionality',
   text:
     'node + express | cors | dotenv | helmet | mongodb + mongoose | chai + chai-http + mocha | prettier | digitalocean + dokku | zombie | git',
   demo: 'https://anon-message-board.andrew-horn-portfolio.life/',
