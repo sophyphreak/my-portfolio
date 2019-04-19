c8ce2080295ab27a264c51c734c718ef25d2e076
Andrew Horn <arhorn@smcm.edu>
2019-04-19 00:16:01 -0500

    updated cooking measure converter text

--- src/components/Projects/ReactApps/individualProjects/cookingMeasureConverter.js
+++ src/components/Projects/ReactApps/individualProjects/cookingMeasureConverter.js
@@ -2,7 +2,7 @@ export default {
   title: 'cooking-measure-converter',
   imagePath: 'reactApps/cooking-measure-converter.png',
   subtitle:
-    'helps you convert metric to imperial measures and vice versa for cooking.',
+    'helps you convert metric to imperial measures and vice versa for cooking',
   text:
     'react | sass | node + express | webpack + babel + next | digitalocean + dokku | bootstrap + reactstrap | jest + enzyme + react-test-renderer | axios | firebase | convert-units | body-parser | html-to-text | moment | round-precision | prettier + husky + lint-staged | git',
   demo: 'https://cooking-measure-converter.andrew-horn-portfolio.life/',
