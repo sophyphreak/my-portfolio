5b3f0039f9df90b35c510a14e9679b7eedd80a4a
Andrew Horn <arhorn@smcm.edu>
2019-04-19 00:21:46 -0500

    edited text description of eog-api

--- src/components/Projects/ApisAndMicroservices/individualProjects/eogApi.js
+++ src/components/Projects/ApisAndMicroservices/individualProjects/eogApi.js
@@ -2,7 +2,7 @@ export default {
   title: 'eog-api',
   imagePath: 'apisAndMicroservices/eog-api.png',
   subtitle:
-    'returns an array of trailing location data for a fake drone slowly circling houston, texas.  updates every four seconds.  this is a clone of the drone api uses for their react assessments',
+    'returns an array of trailing location data for a fake drone slowly circling houston, texas.  updates every four seconds.  this is a clone of the drone api eog resources uses for their react assessments.  you can see the map representation of the latitude/longitude location data in the react apps section',
   text:
     'node + express | moment | husky + lint-staged + prettier | digitalocean + dokku | jade | git',
   demo: 'https://eog-api.andrew-horn-portfolio.life/api/drone/',
