0b8a74477fddc515c8347f06ee39039ad076397f
Andrew Horn <arhorn@smcm.edu>
2019-04-25 23:05:19 -0500

    edited the subtitle for eog react map front and back ends

--- src/components/Projects/ReactApps/individualProjects/eogReactMap.js
+++ src/components/Projects/ReactApps/individualProjects/eogReactMap.js
@@ -2,7 +2,7 @@ export default {
   title: 'eog-react-map',
   imagePath: 'reactApps/eog-react-map.png',
   subtitle:
-    'this app tracks a virtual drone as it slowly circles houston.  you can find the api this app is pinging in the apis and microservices section.  this is my answer to a react assessment i did for eog resources',
+    'this app tracks a virtual drone as it slowly circles houston.  its backend is in the apis and microservices section of this portfolio',
   text:
     'react + redux + redux-saga | dotenv | materialui | google-map-react | moment | prettier + husky + lint-staged | git',
   demo: 'https://andrew-horn-eog-react-map.netlify.com/',
