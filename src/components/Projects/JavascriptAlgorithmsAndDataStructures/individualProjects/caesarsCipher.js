9d1f0a068eb8e034a2f41b3d7636eb76b5fd0278
Andrew Horn <arhorn@smcm.edu>
2019-05-03 10:39:06 -0500

    Fixed link to Caesar's Cipher demo

--- src/components/Projects/JavascriptAlgorithmsAndDataStructures/individualProjects/caesarsCipher.js
+++ src/components/Projects/JavascriptAlgorithmsAndDataStructures/individualProjects/caesarsCipher.js
@@ -4,6 +4,6 @@ export default {
   subtitle: "encode or decode using caesar's cipher",
   text:
     'react + create-react-app | netlify | bootstap + reactstrap | prettier | git',
-  demo: 'https://caesars-cipher-converter-andrew-horn.netlify.com',
+  demo: 'https://caesars-cipher-andrew-horn.netlify.com/',
   github: 'https://github.com/sophyphreak/caesars-cipher'
 };
