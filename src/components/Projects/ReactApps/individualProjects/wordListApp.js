7c306036bb7465b1a3acc1b762a851fde8b41765
Andrew Horn <arhorn@smcm.edu>
2019-07-17 16:22:56 -0500

    updated tech list for wordListApp

--- src/components/Projects/ReactApps/individualProjects/wordListApp.js
+++ src/components/Projects/ReactApps/individualProjects/wordListApp.js
@@ -4,7 +4,7 @@ export default {
   subtitle:
     "a chingu voyage 9 project, built and completed with another developer in dallas. this is a tool to help upper-intermediate and advanced language learners. create a user account; while reading your target language, input words you don't know into the add word page, and then see the word list, ordered by how many times you saw each word on the wordlist page.",
   text:
-    'django + django-rest-framework | react + create-react-app | axios | netlify | bootstrap + reactstrap | prettier + husky + pretty-quick | git',
+    'django + djangorestframework | django-rest-auth + django-allauth | coreapi | pyyaml | django-rest-swagger | django-cors-headers | dokku + digitalocean | react + create-react-app | axios | netlify | bootstrap + reactstrap | prettier + husky + pretty-quick | git',
   demo: 'https://v9-bears-team-13.netlify.com',
   github: 'https://github.com/chingu-voyages/v9-bears-team-13'
 };
