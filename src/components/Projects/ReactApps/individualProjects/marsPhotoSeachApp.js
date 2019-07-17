c85779a80da341b9cbfe5f7d06aa94906b51fe23
Andrew Horn <arhorn@smcm.edu>
2019-07-17 16:37:01 -0500

    updated mars-photo-search-app technologies

--- src/components/Projects/ReactApps/individualProjects/marsPhotoSeachApp.js
+++ src/components/Projects/ReactApps/individualProjects/marsPhotoSeachApp.js
@@ -3,7 +3,8 @@ export default {
   imagePath: 'reactApps/mars-photo-search-app.png',
   subtitle:
     'search the mars photo api for pictures from the curiousity rover. includes a totally needless flask backend',
-  text: 'react + create-react-app | netlify | bootstrap + reactstrap | git',
+  text:
+    'flask | flask-cors | requests | gunicorn | dokku + digitalocean | react + gatsby | axios | formik + yup | query-string | netlify | bootstrap + reactstrap | prettier + husky + pretty-quick | git',
   demo: 'https://andrew-horn-mars-photo-search.netlify.com//',
   github: 'https://github.com/sophyphreak/mars-photo-search-app'
 };
