b8e775cd527f0e5b528b5e9749008b3bf8b0c912
Andrew Horn <arhorn@smcm.edu>
2020-04-01 08:44:30 -0500

    updated href to request-header-parser

--- src/components/Projects/RestApis/individualProjects/requestHeaderParser.js
+++ src/components/Projects/RestApis/individualProjects/requestHeaderParser.js
@@ -3,6 +3,6 @@ export default {
   imagePath: 'restApis/request-header-parser.png',
   subtitle: 'GET some information about your header',
   text: 'node + express | digitalocean + dokku | git',
-  demo: 'https://request-header.andrew-horn-portfolio.life/',
+  demo: 'https://request-header-parser.andrew-horn-portfolio.life/',
   github: 'https://github.com/sophyphreak/request-header-parser'
 };
