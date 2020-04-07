deeff9f7b440ab2990ca0925af33b92b0de8d34d
Andrew Horn <arhorn@smcm.edu>
2020-04-07 16:34:44 -0500

    Added flask-file-metadata

---
+++ src/components/Projects/FlaskApis/individualProjects/flaskFileMetadata.js
@@ -0,0 +1,8 @@
+export default {
+  title: 'flask-file-metadata',
+  subtitle: 'POST your file to get some metadata about it back.',
+  text:
+    'flask + flask-restful | werkzeug | gunicorn | digitalocean + dokku | git',
+  demo: 'https://flask-file-metadata.netlify.com/',
+  github: 'https://github.com/sophyphreak/flask-file-metadata'
+};
--- src/components/Projects/FlaskApis/projectList.js
+++ src/components/Projects/FlaskApis/projectList.js
@@ -1,9 +1,11 @@
+import flaskFileMetadata from './individualProjects/flaskFileMetadata';
 import flaskExerciseTracker from './individualProjects/flaskExerciseTracker';
 import flaskUrlShortener from './individualProjects/flaskUrlShortener';
 import flaskTimestamp from './individualProjects/flaskTimestamp';
 import flaskRequestHeaderParser from './individualProjects/flaskRequestHeaderParser';
 
 export default [
+  flaskFileMetadata,
   flaskExerciseTracker,
   flaskUrlShortener,
   flaskRequestHeaderParser,
