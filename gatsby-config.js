89b68f0ce024dc56bd8ccb83af15070e8f1a67d5
Andrew Horn <arhorn@smcm.edu>
2020-04-06 21:23:23 -0500

    removed image stuff from config file -- to fix netlify error

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,17 +4,5 @@ module.exports = {
     description: `hey, this is my portfolio!`,
     author: `andrew horn`
   },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`
-      }
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    `gatsby-plugin-sass`
-  ]
+  plugins: [`gatsby-plugin-react-helmet`, `gatsby-plugin-sass`]
 };
