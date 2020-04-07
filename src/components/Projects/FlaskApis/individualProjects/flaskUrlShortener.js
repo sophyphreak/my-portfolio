1a467c69d639e27ece93a1dfa9cc4c6a2e6c4a64
Andrew Horn <arhorn@smcm.edu>
2020-04-07 11:43:23 -0500

    corrected link to demo

--- src/components/Projects/FlaskApis/individualProjects/flaskUrlShortener.js
+++ src/components/Projects/FlaskApis/individualProjects/flaskUrlShortener.js
@@ -3,6 +3,6 @@ export default {
   subtitle: 'Shortens urls and redirects.',
   text:
     'flask + flask_restful | flask_sqlalchemy + sqlite3 | gunicorn | digitalocean + dokku | git',
-  demo: 'https://flask-url-shortener.netlfiy.com/',
+  demo: 'https://flask-url-shortener.netlify.com/',
   github: 'https://github.com/sophyphreak/flask-url-shortener'
 };
