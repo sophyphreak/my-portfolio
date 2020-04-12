1c1120a557463d775a55514418d55fc466cb104e
Andrew Horn <arhorn@smcm.edu>
2020-04-11 22:20:15 -0500

    fixed capitalization in wordListApp section

--- src/components/Projects/FullStackApps/individualProjects/wordListApp.js
+++ src/components/Projects/FullStackApps/individualProjects/wordListApp.js
@@ -1,7 +1,7 @@
 export default {
   title: 'word-list-app',
   subtitle:
-    "A chingu voyage 9 project, built and completed with another developer in dallas. this is a tool to help upper-intermediate and advanced language learners. create a user account; while reading your target language, input words you don't know into the add word page, and then see the word list, ordered by how many times you saw each word on the wordlist page.",
+    "A chingu voyage 9 project, built and completed with another developer in dallas. This is a tool to help upper-intermediate and advanced language learners. Create a user account; while reading your target language, input words you don't know into the add word page, and then see the word list, ordered by how many times you saw each word on the wordlist page.",
   text:
     'django + djangorestframework | django-rest-auth + django-allauth | coreapi | pyyaml | django-rest-swagger | django-cors-headers | dokku + digitalocean | react + create-react-app | axios | netlify | bootstrap + reactstrap | prettier + husky + pretty-quick | git',
   demo: 'https://v9-bears-team-13.netlify.com',
