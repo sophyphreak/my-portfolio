7188decdef3327a3b30efa28f48b49e5d96c036e
Andrew Horn <arhorn@smcm.edu>
2020-10-07 17:53:45 -0500

    capitalized some words in homeless-in-houston text

--- src/components/Projects/ReactApps/individualProjects/homelessInHouston.js
+++ src/components/Projects/ReactApps/individualProjects/homelessInHouston.js
@@ -1,7 +1,7 @@
 export default {
   title: 'homeless-in-houston',
   subtitle:
-    "My Chingu Voyage 9 project partner and I decided to partner with the people's church in houston to build an app for finding homeless servcies. The app shows the user all the homeless services in Houston, sorted by how far away the user is from each. It also gives links to google maps directions for each.",
+    "My Chingu Voyage 9 project partner and I decided to partner with the People's Church in Houston to build an app for finding homeless servcies. The app shows the user all the homeless services in Houston, sorted by how far away the user is from each. It also gives links to google maps directions for each.",
   text:
     'react + gatsby | axios | netlify | bootstrap + reactstrap | prettier + husky + pretty-quick | git',
   demo: '',
