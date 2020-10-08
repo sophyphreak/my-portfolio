1c8079d7f2e2fd388af7df672db1b95277b8db14
Andrew Horn <arhorn@smcm.edu>
2020-10-08 18:45:06 -0500

    updated cooking measure converter url

--- src/components/Projects/ReactApps/individualProjects/cookingMeasureConverter.js
+++ src/components/Projects/ReactApps/individualProjects/cookingMeasureConverter.js
@@ -4,6 +4,6 @@ export default {
     'Helps you convert metric to imperial measures and vice versa for cooking.',
   text:
     'react + gatsby | node-sass | bootstrap + reactstrap | jest + enzyme + react-test-renderer | convert-units + round-precision | prettier + pretty-quick + husky | git',
-  demo: 'https://cookingmeasure.net/',
+  demo: 'https://andrew-horn-cooking-measure-converter.netlify.app/',
   github: 'https://github.com/sophyphreak/cooking-measure-converter'
 };
