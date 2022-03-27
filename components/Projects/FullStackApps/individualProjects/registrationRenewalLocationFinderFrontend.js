9a3215ce94e3b4cc72b0503722794f40353c84b6
Andrew Horn <arhorn@smcm.edu>
2022-03-27 00:29:35 -0500

    Removed demo because it's broken right now

--- components/Projects/FullStackApps/individualProjects/registrationRenewalLocationFinderFrontend.js
+++ components/Projects/FullStackApps/individualProjects/registrationRenewalLocationFinderFrontend.js
@@ -2,6 +2,6 @@ export default {
   title: 'registration-renewal-location-finder-frontend',
   subtitle:
     'Find where you can renew your car registration on the same day in Harris County, Texas.',
-  demo: 'https://harriscarrenewal.netlify.app/',
+  demo: '',
   github: 'https://github.com/sophyphreak/car-renewal-frontend',
 };
