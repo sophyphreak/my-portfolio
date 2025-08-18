ba8a58838898ed4f7816f553f084ead43cf7e1fc
Andrew Horn <arhorn@smcm.edu>
2025-08-18 11:48:04 -0400

    added technologies tab

--- components/Projects/Projects.js
+++ components/Projects/Projects.js
@@ -5,12 +5,14 @@ import FlaskApis from './FlaskApis/FlaskApis';
 import NodeApis from './NodeApis/NodeApis';
 import Go from './Go/Go';
 import { Tabs, TabList, Tab, TabPanels, TabPanel } from '@chakra-ui/react';
+import Techonologies from '../Technologies';
 
 const Projects = () => {
   return (
     <div>
       <Tabs align="center">
         <TabList>
+          <Tab>Technologies</Tab>
           <Tab>Frontend React</Tab>
           <Tab>Full Stack React</Tab>
           <Tab>Python</Tab>
@@ -19,6 +21,9 @@ const Projects = () => {
         </TabList>
 
         <TabPanels>
+          <TabPanel>
+            <Techonologies />
+          </TabPanel>
           <TabPanel>
             <ReactApps />
           </TabPanel>
---
+++ components/Technologies.tsx
@@ -0,0 +1,29 @@
+import type { NextPage } from 'next';
+
+const Technologies: NextPage = () => {
+  return (
+    <section className="tech">
+      <h2 className="tech__title">Technologies</h2>
+      <div className="tech-container">
+        <p>
+          JavaScript - TypeScript - Python - Go - Java - HTML5 - CSS3 - SQL -
+          React - Redux - Redux Thunk - Redux Saga - React Router - React Hook
+          Form - Formik - Gatsby - Next.js - Material-UI - Chakra UI - Bootstrap
+          - Reactstrap - Semantic UI React - D3 - React Data Grid - Jest - React
+          Testing Library - Mock Service Worker - Cypress - Node.js - Express -
+          Socket.IO - Passport - FastAPI - Flask - Flask-RESTful - Django -
+          Django REST Framework - Puppeteer - Playwright - MongoDB - Mongoose -
+          PyMongo - Prisma - Axios - node-fetch - npm - Yarn - pnpm - Webpack -
+          Babel - ESLint - Prettier - Stylelint - Storybook - Husky -
+          lint-staged - ts-jest - Babel Jest - TypeScript config - GitHub
+          Actions - Docker - Netlify - Heroku - Sass - SCSS - PostCSS - Emotion
+          - Styled-components - Tailwind CSS - Astro - Docz - Go modules -
+          Gradle - Firebase - PDF.js - Agile - SAFe - Scrum - Netlify - Vercel -
+          Cursor - Claude Code - GPT-5 - git - Github
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Technologies;
