b40a914527ce376d5d5632c5dace7d91a59532f5
Andrew Horn <arhorn@smcm.edu>
2025-12-12 15:21:35 -0500

    added more about me, contact page link

--- components/AboutMe/AboutMe.js
+++ components/AboutMe/AboutMe.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import { Text } from '@chakra-ui/react';
+import { Link,Text } from '@chakra-ui/react';
 
 const AboutMe = () => {
   return (
     <section id="about-me">
       <Text align="center" fontSize="5xl" my="1em">
-        Hi, I&apos;m Andrew Horn. Welcome to my personal portfolio page!
+        Hi, I&apos;m Andrew Horn, owner of Karuna Code LLC.
       </Text>
+      <Text align="center" fontSize="2xl" mx="2em" mb="2em">
+        I am a full-stack web developer with a passion for creating efficient and scalable applications. With experience in both front-end and back-end technologies, I enjoy building seamless user experiences and robust server-side solutions. When I&apos;m not coding, I love writing and organizing.
+      </Text>
+            <Text align="center" fontSize="2xl" mx="2em" mb="2em"><Link href="/contact" color="teal.500" textDecoration="underline">
+Contact me here      </Link></Text>
     </section>
   );
 };
